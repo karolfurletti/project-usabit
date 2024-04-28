34133e10b298f7ed66184e9dbac0334fdc778a38
Karoline Oliveira <karolfurletti@gmail.com>
2024-04-27 22:12:38 -0400

    feat: add msw and zod in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {setupWorker} from "msw/browser";
+import { handlers } from './mocks/handlers';
 function App() {
+
+
+    useEffect(() => {
+        const Worker = setupWorker(...handlers);
+        void Worker.start({ onUnhandledRequest: 'bypass' });
+    }, []);
+
+
   const [count, setCount] = useState(0)
 
   return (
